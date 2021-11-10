fd71b0419b70d45db7639a36f9063ac6976b85b4
levchenko-yarya <levchenkoyarya@gmail.com>
2021-11-10 18:31:52 +0200

    form add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import {Component} from '@angular/core'
 
 @Component({
     'selector': 'my-app',
-    'template': `<label>Введите имя</label>
-    <input type="text" placeholder="name">
-    <h1>добро пожаловать {{name}}</h1>`
+    'templateUrl': 'app.component.html'
 })
 
 export class AppComponent {
-    name = ''
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {FormsModule} from '@angular/forms'
 import {AppComponent} from './app.component'
+import {FormModule} from "./form/form.module"
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, FormModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
