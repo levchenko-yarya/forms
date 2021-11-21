48848f15cb8a98fe24c6138bc831c3888f85d710
levchenko-yarya <levchenkoyarya@gmail.com>
2021-11-21 22:54:53 +0200

    service refactor

---
+++ src/app/form/builder/builder.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core"
+import {HttpClient} from "@angular/common/http"
+import {Observable} from "rxjs"
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class BuilderService {
+
+    private url = 'http://localhost:3000/builder';
+
+    constructor(private http: HttpClient) {
+    }
+
+    getBuilders(): Observable<any> {
+        return this.http.get(`${this.url}/all`)
+    }
+
+    getBuilder(id: number): Observable<any> {
+        return this.http.get(`${this.url}/${id}`)
+    }
+
+    createBuilder(builder: Object): Observable<Object> {
+        return this.http.post(`${this.url}/`, builder)
+    }
+
+    updateBuilder(id: number, value: any): Observable<Object> {
+        return this.http.put(`${this.url}/${id}`, value)
+    }
+
+    deleteBuilder(id: number): Observable<Object> {
+        return this.http.delete(`${this.url}/${id}`)
+    }
+}
--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -8,37 +8,28 @@ import {Observable} from "rxjs"
 
 export class FormService {
 
-    private url = 'http://localhost:3000';
+    private url = 'http://localhost:3000/form';
 
     constructor(private http: HttpClient) {
     }
 
     getForms(): Observable<any> {
-        return this.http.get(`${this.url}/forms`)
+        return this.http.get(`${this.url}/all`)
     }
 
     getForm(id: number): Observable<any> {
-        return this.http.get(`${this.url}/form/${id}`)
+        return this.http.get(`${this.url}/${id}`)
     }
 
     createForm(form: Object): Observable<Object> {
-        return this.http.post(`${this.url}/form`, form)
+        return this.http.post(`${this.url}`, form)
     }
 
     updateForm(id: number, value: any): Observable<Object> {
-        return this.http.put(`${this.url}/form/${id}`, value)
+        return this.http.put(`${this.url}/${id}`, value)
     }
 
     deleteForm(id: number): Observable<Object> {
-        return this.http.delete(`${this.url}/form/${id}`)
+        return this.http.delete(`${this.url}/${id}`)
     }
-
-    getBuilders(): Observable<any> {
-        return this.http.get(`${this.url}/builders`)
-    }
-
-    createBuilder(builder: Object): Observable<Object> {
-        return this.http.post(`${this.url}/builder`, builder)
-    }
-
 }
